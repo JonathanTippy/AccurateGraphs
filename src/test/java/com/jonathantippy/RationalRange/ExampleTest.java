3c28b459542571e87049fff668db0c1dd6c9e991
JonathanTippy <jonathan@tippy.name>
2024-12-31 12:19:37 -0600

    start from RationalRange

---
+++ src/test/java/com/jonathantippy/RationalRange/ExampleTest.java
@@ -0,0 +1,12 @@
+package com.jonathantippy.RationalRange;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ExampleTest {
+    @Test
+    public void testTests() {
+        int total = 1 + 1;
+        assertEquals(2, total);
+    }
+}
